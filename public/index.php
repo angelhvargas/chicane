28ff124260a0413b1a2a921a0a8981a1aee4f5d6
Angel Vargas <angelvargas@outlook.es>
2018-02-13 23:25:10 +0000

    First commit: defining path to take about the framework design

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Chicane - A PHP Framework For Web Artisans
+ *
+ * @package  Chicane
+ * @author   Angel Vargas <angelvargas@outlook.es>
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+|
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels nice to relax.
+|
+*/
+
+echo "hello";
+var_dump( __DIR__.'/../bootstrap/autoload.php');
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Resolve Request and execute app action
+|--------------------------------------------------------------------------
+|
+|
+|
+*/
+
+
