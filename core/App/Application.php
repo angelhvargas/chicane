eab1a87267e144eb499a829883f76e59179ccdc0
Angel Vargas <angelvargas@outlook.es>
2018-03-14 08:29:11 +0000

    added new logic to handle a kind of middleware

--- core/App/Application.php
+++ core/App/Application.php
@@ -4,43 +4,69 @@ namespace Chicane;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class Application implements HttpKernelInterface {
     
     protected $app_path;
     protected $instances;
     protected $routes;
+    protected $dispatcher;
 
     public function __construct(string $app_path) 
     {
         $this->path = $app_path;
+        $this->routes = new RouteCollection();
+        $this->dispatcher = new EventDispatcher();
     }
 
-    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true) {
-        
-        $path = $request->getPathInfo();
-			
-        // Does this URL match a route?
-        if (array_key_exists($path, $this->routes)) {
-            // execute the callback
-            $controller = $routes[$path];
-            $response = $controller();
-        } else {
-            // no route matched, this is a not found.
-            $response = new Response('Not found!', Response::HTTP_NOT_FOUND);
+    public function handle(Request $request, $type = self::MASTER_REQUEST, $catch = true) 
+    {
+        $event = new \Chicane\Events\RequestEvent();
+        $event->setRequest($request);   
+        $this->dispatcher->dispatch($event);
+        $context = new \Symfony\Component\Routing\RequestContext();
+        $context->fromRequest($request);
+        $matcher = new \Symfony\Component\Routing\Matcher\UrlMatcher($this->routes, $context);  
+
+        try {
+            $attributes = $matcher->match($request->getPathInfo());
+            $controller = $attributes['controller'];
+            unset ($attributes['controller']);
+            $response = call_user_func($controller, $attributes);
+
+            $response = $controller;
+        } catch(\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
+            $response = new Response('Not found!!!', Response::HTTP_NOT_FOUND);
         }
 
         return $response;
     }
+    //add new route maps to the framework
+    public function map($path, $controller)
+    {
+        $this->routes->add($path, new Route(
+            $path,
+            ['controller' => $controller]
+        ));
+    }
 
     // Associates an URL with a callback function
-	public function map($path, $controller) {
-		$this->routes[$path] = $controller;
-	}
 
     public function appPath()
     {
         return $this->app_path;
+    }
+
+    public function on($event, $callback)
+    {
+        $this->dispatcher->addListener($event, $callback);
+    }
+
+    public function fire($event) 
+    {
+        return $this->dispatcher->dispatch($event);
     }
 
     public function registerInstance($instance) {
