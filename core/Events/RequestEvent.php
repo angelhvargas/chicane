c2ab5a3d8e876730ab10b34e9841d75536efcede
Angel Vargas <angelvargas@outlook.es>
2018-03-15 08:41:15 +0000

    added getter function on class

--- core/Events/RequestEvent.php
+++ core/Events/RequestEvent.php
@@ -5,9 +5,15 @@ namespace Chicane\Events;
 class RequestEvent extends \Symfony\Component\EventDispatcher\Event {
     
     protected $request;
-$) 
+
+    public function setRequest($request) 
     {
         $this->request = $request;
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
     }
 
 }
