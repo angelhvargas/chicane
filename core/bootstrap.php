423f61221907c61eb43ec3c7a80e32fd377d0ea4
Angel Vargas <angelvargas@outlook.es>
2018-04-04 22:57:13 +0100

    added ORM config

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,6 +1,7 @@
 <?php
 
 define('CHICANE_START', microtime(true));
+define('app', null);
 
 /*
 |--------------------------------------------------------------------------
@@ -11,11 +12,27 @@ define('CHICANE_START', microtime(true));
 
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->register();
+
+use Doctrine\ORM\Tools\Setup;
+use Doctrine\ORM\EntityManager;
+
+$paths = array(__DIR__.'/../app/entities');
+$isDevMode = false;
+
+// the connection configuration
+$dbParams = array(
+    'driver'   => 'pdo_mysql',
+    'user'     => 'root',
+    'password' => '1234567890',
+    'dbname'   => 'chicane',
+);
+
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$entityManager = EntityManager::create($dbParams, $config);
 /*
 |--------------------------------------------------------------------------
 | Start Application
 |--------------------------------------------------------------------------
 |
 */
-
 require_once __DIR__.'/../core/start.php';
