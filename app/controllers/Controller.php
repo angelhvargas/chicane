8d5d6552abd44d52737baeca0fee1cebbb0921fd
Angel Vargas <angelvargas@outlook.es>
2018-04-04 22:55:21 +0100

    Added controller

---
+++ app/controllers/Controller.php
@@ -0,0 +1,34 @@
+<?php
+namespace Chicane;
+
+class Controller {
+
+    protected $model;
+    
+    public function index() 
+    {
+        //todo
+    }
+
+    public function create() 
+    {
+        //todo
+    }
+
+    public function get($id) 
+    {
+        //todo
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    
+}
