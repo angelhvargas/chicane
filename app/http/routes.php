440cb1588e07e26b9a70fa580074c01db4bd8e45
Angel Vargas <angelvargas@outlook.es>
2018-04-18 12:59:50 +0100

    Updated routes with couple closures as example.

--- app/http/routes.php
+++ app/http/routes.php
@@ -1,12 +1,24 @@
 <?php
 
 
-/*
- *Routes file ----
- * Add any routes to this file.
- * Please check documentation
- *
+/**
+ * Routes
  */
-$app->map('/hello/{name}', function ($name) {
-    return new \Symfony\Component\HttpFoundation\Response(phpinfo());
+
+$app->map('/welcome/{name}', function ($name) {
+    return new \Symfony\Component\HttpFoundation\Response("hello ".$name. " welcome to the Chicane Framework");
+});
+
+$app->map('/', function() use ($app) {
+    
+    return new \Symfony\Component\HttpFoundation\Response(
+        $app
+        ->getInstance('view')
+        ->render(
+            'index.twig', 
+            [
+                'hello' => 'This is a message send from the back-end, you can see me at routes.php'
+            ]
+        )
+    );   
 });
