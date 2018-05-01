d2dd95ff5b509e712cdd9aa16582850be75f3623
Angel Vargas <angelvargas@outlook.es>
2018-05-01 22:19:24 +0100

    added event service provider

---
+++ core/App/Events/EventServiceProvider.php
@@ -0,0 +1,20 @@
+<?php namespace Chicane\Events;
+
+use Chicane\Base\ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider {
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['events'] = $this->app->share(function($app)
+		{
+			return new Dispatcher($app);
+		});
+	}
+
+}
